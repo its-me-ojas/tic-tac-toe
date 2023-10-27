1f62ef0ce811d93085e9340700f3d359ae0baacc
Stvrlightt <crestfallenwizard@gmail.com>
2023-10-27 18:05:17 +0530

    refractor

--- src/App.jsx
+++ src/App.jsx
@@ -53,9 +53,9 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const xIsNext = currentMove % 2 === 0
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
@@ -65,7 +65,7 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-    setCurrentMove(nextMove)
+    setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
